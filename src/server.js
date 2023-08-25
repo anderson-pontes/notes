4af3a97706ffd110bad880f09808bff331aefc6e
Anderson Pontes <andersonpontes.dev@gmail.com>
2023-08-25 18:24:10 -0300

    Query Params

--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ app.get("/message/:id/:user", (request, response) => {
     response.send(`Mensagem ID: ${id}. Para o usuário: ${user}.`);
 });
 
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Página: ${page}. Mostrar: ${limit}`);
+});""
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server in running on Porta ${PORT}`));
