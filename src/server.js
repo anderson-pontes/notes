cba1de99a68be8d9da128683f7f9755b794c8faa
Anderson Pontes <andersonpontes.dev@gmail.com>
2023-08-25 18:54:32 -0300

    "Método POST"

--- src/server.js
+++ src/server.js
@@ -2,16 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {
+/*app.get("/message/:id/:user", (request, response) => {
     const { id, user } = request.params;
 
     response.send(`Mensagem ID: ${id}. Para o usuário: ${user}.`);
-});
+});*/
 
-app.get("/users", (request, response) => {
-    const { page, limit } = request.query;
+app.post("/users", (request, response) => {
 
-    response.send(`Página: ${page}. Mostrar: ${limit}`);
+   // const { page, limit } = request.query;
+   response.send(`Você chamou o POST`);
 });""
 
 const PORT = 3000;
