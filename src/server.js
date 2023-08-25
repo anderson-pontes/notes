8afa587ba08c07f71c0def6cddcace2b82e021fb
Anderson Pontes <andersonpontes.dev@gmail.com>
2023-08-25 19:11:43 -0300

    "configurando o POST para o json"

--- src/server.js
+++ src/server.js
@@ -2,17 +2,14 @@ const express = require("express");
 
 const app = express();
 
-/*app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params;
-
-    response.send(`Mensagem ID: ${id}. Para o usuário: ${user}.`);
-});*/
+app.use(express.json());
 
 app.post("/users", (request, response) => {
 
-   // const { page, limit } = request.query;
-   response.send(`Você chamou o POST`);
-});""
+const { name, email, password } = request.body;
+
+   response.json({ name, email, password });
+});
 
 const PORT = 3000;
 
