78c1ab27b42ebab4ad2993b72e60020b63b9eeb0
Anderson Pontes <andersonpontes.dev@gmail.com>
2023-08-26 00:31:27 -0300

    "Trantando Exceções"

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -5,10 +5,17 @@ create - POST para criar um registro.
 update - PUT para atualizar um registro.
 delete - DELETE para remover um registro.
  */
+const AppError = require("../utils/AppError")
+
 class UsersController {
     create(request, response) {
 
         const { name, email, password } = request.body;
+
+        if (!name) {
+            throw new AppError("Nome é obrigatório!");            
+        }
+
 
         response.status(201).json({ name, email, password });
     }
