ff652eb66795eaee6c4202088e21e39b8302fb47
Anderson Pontes <andersonpontes.dev@gmail.com>
2023-08-25 21:52:26 -0300

    "Organizando o Projeto"

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const {} = require("./routes/users.routes")
+const routes = require("./routes");
 
 const app = express();
-
 app.use(express.json());
 
-const PORT = 3000;
+app.use(routes);
 
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server in running on Porta ${PORT}`));
